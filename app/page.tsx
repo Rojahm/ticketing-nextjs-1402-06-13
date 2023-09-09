ce45875716c1b679436e2a05e5cfce4859e0035f
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-09-09 21:12:27 +0330

    head

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ export default function Home() {
   return (
     <main className="">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
-      <div className="flex">
-        <h2>hi</h2>
-        <div className="flex-none">01</div>
-        <div className="flex-auto w-64">02</div>
-        <div className="flex-auto w-32">03</div>
+      <div className="flex h-96 ">
+        <div className="flex-none bg-slate-400/50">01</div>
+        <div className="flex-auto w-64 bg-slate-400">02</div>
+        <div className="flex-auto w-32 bg-slate-400/50">03</div>
       </div>
     </main>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import { GrLogin } from "react-icons/gr";
 
 const Header = () => {
   return (
     <div className="flex justify-around justify-items-center">
       <div className="inline-flex justify-between">
-        <button className="rounded-full h-10 px-5 hover:border hover:shadow hover:border-orange-400 active:border-2">
-          login
+        <div className="mt-16 pr-16 hover:cursor-pointer">
+          <GrLogin style={{ fontSize: "30px", color: "#487gv" }} />
+        </div>
+        <button className="text-lg text-white rounded-full bg-yellow-500 w-64 h-10 px-5 hover:shadow hover:border-orange-400 active:border-2 mt-14">
+          ارسال تیکت
         </button>
-        <button>ارسال تیکت</button>
       </div>
       <Image
         src={
