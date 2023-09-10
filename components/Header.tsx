aab39e4ac7a94b37db36ce3967ec9952017bcda9
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-09-10 16:09:00 +0330

    theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,7 @@ import { GrLogin } from "react-icons/gr";
 
 const Header = () => {
   return (
-    <div className="flex justify-around justify-items-center">
-      <div className="inline-flex justify-between">
-        <div className="mt-16 pr-16 hover:cursor-pointer">
-          <GrLogin style={{ fontSize: "30px", color: "#487gv" }} />
-        </div>
-        <button className="text-lg text-white rounded-full bg-yellow-500 w-64 h-10 px-5 hover:shadow hover:border-orange-400 active:border-2 mt-14">
-          ارسال تیکت
-        </button>
-      </div>
+    <div className="Header flex justify-around justify-items-center">
       <Image
         src={
           "/img/lssi-high-resolution-logo-color-on-transparent-background.webp"
@@ -21,6 +13,12 @@ const Header = () => {
         alt="lssi it logo"
         className="rounded drop-shadow-sm shadow py-3 hover:cursor-pointer hover:drop-shadow-md hover:shadow-transparent"
       />
+      <div className="inline-flex justify-between">
+        <button className="ticket-btn">ارسال تیکت</button>
+        <div className="mt-16 pl-16 hover:cursor-pointer">
+          <GrLogin style={{ fontSize: "30px", color: "#487gv" }} />
+        </div>
+      </div>
     </div>
   );
 };
