1cfffa927db4184646132370f89cf24de12e8cc2
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-09-10 14:57:01 +0330

    bg gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
-      <div className="flex h-96 ">
-        <div className="flex-none bg-slate-400/50">01</div>
-        <div className="flex-auto w-64 bg-slate-400">02</div>
-        <div className="flex-auto w-32 bg-slate-400/50">03</div>
+    <main>
+      <h1>HomePage</h1>
+      <div className="flex">
+        <div>
+          <Link href={"/"}>Ticket</Link>
+        </div>
       </div>
     </main>
   );
