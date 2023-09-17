3c5c993a9bd1abd5a96424a2ffac886f04b73ec6
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-09-17 23:13:05 +0330

    emoji

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="Footer rainbow mt-5 text-center pt-3">
       <h4>واحد فناوری اطلاعات موسسه کار و تامین اجتماعی</h4>
-      <p className="credit">OpenSource coded by Ro?a</p>
+      <p className="credit">👩‍💻 OpenSource coded by Ro?a 🐱‍👤</p>
     </div>
   );
 };
